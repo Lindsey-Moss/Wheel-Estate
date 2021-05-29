15ef29043fc039fd70ac02fdc36b256078f70c5e
Lindsey Moss <llmodbls@gmail.com>
2021-05-29 12:11:05 -0400

    dependencies installed, scaffold directories, files, and boilerplates added

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import axios from 'axios'
+import { BASE_URL, IMAGE_PATH } from './globals'
+
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+  return (
+    <div className="App">
+      
+      
+      
+    </div>
+  );
+}
+}
---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const ParkingSpot = require('../models/parkingspot')
+
+// get all listings
+
+// post new listing
+
+// put update listing
+
+// delete existing listing
+
+
+module.exports = {
+  // functions go here
+}
