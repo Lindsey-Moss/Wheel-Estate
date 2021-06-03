981f945f0abf30fdbfc3d0bd1dfeb88c8496ca12
Lindsey Moss <llmodbls@gmail.com>
2021-06-03 17:15:39 -0400

    connection correction, again

--- client/src/globals.js
+++ client/src/globals.js
@@ -4,8 +4,8 @@
 
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? `${window.location.origin}`
-    : 'http://localhost:3001'
+    ? `${window.location.origin}/api`
+    : 'http://localhost:3001/api'
 
 
 export const IMAGE_PATH = '#'
