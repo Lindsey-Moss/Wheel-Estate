15b778854c32d5264eaf4678f784e7140ccd8f0c
Lindsey Moss <llmodbls@gmail.com>
2021-06-04 14:24:05 -0400

    Added seed data, style tweaks, UX improvements

--- models/spot.js
+++ models/spot.js
@@ -6,16 +6,16 @@ const SpotSchema = new Schema(
     title: { type: String, required: true},
     owner_name: { type: String, required: true },
     owner_email: { type: String, required: true },
-    price: { type: Number, required: true },
-    image: { type: String, required: true },
-    available_spots: { type: Number, required: true},
-    size_length: {type: Number, required: true},
-    size_width: {type: Number, required: true},
-    utilities: {type: String, enum: ['Electric', 'Water', 'Internet', 'electric', 'water', 'internet', 'None', 'none'], required: true},
+    price: { type: String},
+    image: { type: String},
+    available_spots: { type: String},
+    size_length: {type: String},
+    size_width: {type: String},
+    utilities: {type: String, enum: ['Electric', 'Water', 'Internet', 'electric', 'water', 'internet', 'None', 'none']},
     description: { type: String, required: true },
-    privacy: {type: String, required: true},
-    pets: {type: String, required: true},
-    location: {type: Number, required: true}
+    privacy: {type: String},
+    pets: {type: String},
+    location: {type: String, required: true}
   },
   { timestamps: true }
 )
